d31fc6fb753cda04e6a8ffcb880dc13b9b9373f4
nicoleonard <nikolasleonard@gmail.com>
2023-06-09 23:42:35 -0300

    agregado llamado a la api de noticias con parametro de categorias y pase de respuesta mediante props a ListaNoticias

--- src/components/GeneradorNoticias.jsx
+++ src/components/GeneradorNoticias.jsx
@@ -21,7 +21,9 @@ const GeneradorNoticias = () => {
 
     const consultarNoticias = async (categoria) =>{
         try{
-            console.log(categoria)
+            const respuesta = await fetch (`https://newsdata.io/api/1/news?apikey=pub_24276a6d001c4c2d36427a7685dbaf9188b90&language=es&category=${categoria}`)
+            const datos = await respuesta.json()
+            setNoticias(datos)
         }catch(error){
             console.log(error)
         }
@@ -33,10 +35,10 @@ const GeneradorNoticias = () => {
             <Form className='m-2 p-2 border border-2 border-dark-subtle'>
                 <Form.Group>
                     <Form.Select onChange={(evento) => manejadorSelector(evento)} defaultValue={categoriaGuardada}>
-                        <option value="ultimas noticias">Ultimas noticias</option>
-                        <option value="ciencia">Ciencia</option>
-                        <option value="deportes">Deportes</option>
-                        <option value="politica">Politica</option>
+                        <option value="top">Ultimas noticias</option>
+                        <option value="science">Ciencia</option>
+                        <option value="sports">Deportes</option>
+                        <option value="politics">Politica</option>
                     </Form.Select>
                 </Form.Group>
             </Form>
