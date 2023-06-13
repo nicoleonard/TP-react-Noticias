dc35845f4e3ae21792acd1f8013dc071949a9fad
nicoleonard <nikolasleonard@gmail.com>
2023-06-13 03:37:00 -0300

    agregado titulo, descripcion y link a las targetas de noticias. agregado spinner durante la carga

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -1,11 +1,20 @@
 import Row from 'react-bootstrap/Row';
+import Spinner  from 'react-bootstrap/Spinner';
 import Noticia from './Noticia.jsx';
+import { Container } from 'react-bootstrap';
 
 
 const ListaNoticias = ({propNoticias})=>{
     if(propNoticias.results === undefined){
         return (
-            <Row className='mx-0' xs={1} sm={2} md={3} lg={4} xl={5}>{console.log("cargando")} </Row>)
+            <Container className='d-flex justify-content-center pt-5'>
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </Container>
+
+
+            )
     }else{
         return(
             <Row className='mx-0' xs={1} sm={2} md={3} lg={4} xl={5}>  
--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,14 +1,20 @@
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
 const Noticia = ({propNoticia}) => {
     return (
         <Card className="px-0">
             <Card.Header>
-            
+                <h3>{propNoticia.title}</h3>   
             </Card.Header>
             <Card.Body>
-                
+                <h6>Resumen</h6>
+                {propNoticia.description}
             </Card.Body>
+            <Card.Footer>
+
+                <Button variant="primary" href={propNoticia.link}>Ver más</Button>
+                
+            </Card.Footer>
         </Card>
     );
 };
